c49e10f5861382f6b74f3e668d0df3b70f90f093
Nathan Jodoin <nathanjodoin@my.unt.edu>
2023-04-08 13:01:33 -0500

    Added main.c and associated header with some expected useful includes.

---
+++ main.c
@@ -0,0 +1,11 @@
+// CSCE 3600 - major 2 - group 6
+// main.c
+// author:  Nathan Jodoin
+// date:    08APR23
+// desc:    main function definition and basic shell logic
+
+#include "main.h"
+
+int main( int cargs, char** argv ) {
+  return 0;
+}
---
+++ main.h
@@ -0,0 +1,15 @@
+// CSCE 3600 - major 2 - group 6
+// main.h
+// author:  Nathan Jodoin
+// date:    08APR23
+// desc:    main header
+
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdlib.h>
+#include <stdbool.h>
+#include <signal.h>
+#include <stdio.h>
+
+#endif
