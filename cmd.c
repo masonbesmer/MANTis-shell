b33adb951d9cdebf144a3bca1326658cfb46b01b
MASON-PC\mason <masonbesmer@gmail.com>
2023-04-17 13:50:34 -0500

    goodbye old branch, sig handling done

--- cmd.c
+++ cmd.c
@@ -27,7 +27,7 @@ int shell_cmd(char **args){
         return 0;
     }
     else if (strcmp(args[0], "test") == 0){
-        printf("testing. we're about to fork\n");
+        printf("PARENT: testing. we're about to fork\n");
         pid_t pid;
         pid = fork();
         if (pid == 0){
@@ -37,31 +37,39 @@ int shell_cmd(char **args){
             // respect signals in child
             signal(SIGINT, SIG_DFL);
             signal(SIGTSTP, SIG_DFL);
+            signal(SIGTTOU, SIG_DFL);
 
             // temp code to not terminate until i tell it to
-            printf("just forked, this is the child talking\n");
+            printf("CHILD: just forked\n");
             for(int i = 0; i < 5; i++){
-                printf("child\n");
+                printf("CHILD: child\n");
                 fflush(stdout);
                 sleep(1);
             }
+            printf("CHILD: child done\n");
 
-            // exit child normally (it wont get here) (probably)
             exit(0);
         } else if (pid < 0){
             // fork failed
             perror("ERROR: fork failed");
             return -1;
         } else {
-            printf("place child in foreground, move parent to background\n");
+            printf("PARENT: place child in foreground\n");
             tcsetpgrp(STDIN_FILENO, pid);
 
-            printf("wait for child to finish its stuff, then return to shell\n");
-            waitpid(pid, NULL, 0);
+            //ignore term for writing while in background
+            signal(SIGTTOU, SIG_IGN);
+            printf("PARENT: place parent in background\n");
+            tcsetpgrp(STDIN_FILENO, getpid());
 
-            printf("place parent back in foreground after child is done\n");
-            tcsetpgrp(STDIN_FILENO, getpgid(0));
-            printf("child is done, parent is back in foreground, returning to shell\n");
+            printf("PARENT: wait for child to finish its stuff\n");
+            int status;
+            waitpid(pid, &status, WUNTRACED);
+
+            printf("PARENT: place parent back in foreground\n");
+            tcsetpgrp(STDIN_FILENO, getpgrp());
+            signal(SIGTTOU, SIG_DFL);
+            printf("PARENT: returning to shell\n");
 
             // return back to shell loop
             return 0;
