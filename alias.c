17006b95df9d591a1e543e9b35d87c7005d70066
MASON-PC\mason <masonbesmer@gmail.com>
2023-04-18 16:18:38 -0500

    more work on alias

--- alias.c
+++ alias.c
@@ -1,18 +1,56 @@
 #include "alias.h"
 
 int add_alias(char **iargs) {
-    char *alias = strtok(iargs[1], "="); // split the string at "="
-    char **command = (char**)malloc(sizeof(char*) * 100);
-    int i = 0;
-    char *token = strtok(NULL, "="); // get the part after "="
-    while (token != NULL) {
-        command[i] = token;
-        printf("command[%d]: %s\n", i, command[i]);
-        i++;
-        token = strtok(NULL, "=");
+    //print all of iargs
+    for (int i = 0; i < 100; i++) {
+        if (iargs[i] == NULL) {
+            break;
+        }
+        printf("iargs[%d]: %s\n", i, iargs[i]);
     }
-    printf("Alias: %s\n", alias);
-    printf("Command: %s\n", command[0]);
+
+    char *alias_name = strtok(iargs[1], "="); // get the part before "="
+    char *cmd_arg_1 = strtok(NULL, "\'"); //get token after first "'"
+    iargs[0] = alias_name;
+    iargs[1] = cmd_arg_1;
+
+    for (int i = 0; i < 100; i++) {
+        if (iargs[i+1] == NULL) {
+            iargs[i] = strtok(iargs[i], "\'"); //remove last ' from last argument
+            break;
+        }
+    }
+    //copy iargs to alias_def
+    char **alias_def = (char**)malloc(sizeof(char*) * 100);
+    for (int i = 0; i < 100; i++) {
+        if (iargs[i] == NULL) {
+            break;
+        }
+        alias_def[i] = iargs[i];
+    }
+    //shift alias_def to the left by 1 and remove the first element and store in iargs
+    // for (int i = 0; i < 100; i++) {
+    //     if (alias_def[i+1] == NULL) {
+    //         break;
+    //     }
+    //     iargs[i] = alias_def[i+1];
+    // }
+    for (int i = 0; i < 100; i++) {
+        if (alias_def[i] == NULL) {
+            //printf("alias_def[%d] is NULL\n", i);
+            break;
+        }
+        printf("arr to store for alias def = alias_def[%d]: %s\n", i, alias_def[i]);
+    }
+    printf("gets here?\n");
+    for (int i = 0; i < 100; i++) {
+        if (iargs[i] == NULL) {
+            //printf("iargs[%d] is NULL", i);
+            break;
+        }
+        printf("arr to pass to exec = iargs[%d]: %s\n", i, iargs[i]);
+    }
+    printf("return");
     return 0;
 }
 
