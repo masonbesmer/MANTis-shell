02ef20cb5816fab20c578b33ac249ed48a673b63
Nathan Jodoin <nathanjodoin@my.unt.edu>
2023-04-19 17:06:23 -0500

    Started pipe.c

---
+++ pipe.c
@@ -0,0 +1,14 @@
+// CSCE 3600 - major 2 - group 6
+// pipe.c
+// author:  Nathan Jodoin
+// date:    19APR23
+// desc:    pipe functional definition
+
+int shell_pipe_cmd( char* args[] ) {
+  int num_pipes = 0;
+  // iterator over args array, validate syntax and count number of pipes
+
+  // if syntax valid iteratively exec with stdout of cmd1 to std in of cmd2
+
+  return 0;
+}
